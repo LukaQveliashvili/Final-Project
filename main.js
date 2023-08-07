48cdabd178470910727cef6dc6d5cd82c7d783da
Luka <79097560+LukaQveliashvili@users.noreply.github.com>
2023-08-08 01:49:05 +0400

    Final

--- main.js
+++ main.js
@@ -3,3 +3,32 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("open");
+};
+
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+galleryImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = image.src;
+  });
+});
+
+function closeLightbox() {
+  lightbox.style.display = "none";
+  lightboxImg.src = "";
+}
